981e70e2da7350a5d50ad59d0de5c1757b034ff3
MuhammadFebriArdiyanSaputra <m.febri123.cc@gmail.com>
2024-10-03 23:25:04 +0700

    pembaruan 5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,8 +7,29 @@ use App\Models\UserModel;
 use App\Http\Requests\UserRequest;
 use App\Models\Kelas;
 
+
 class UserController extends Controller
 {
+    public $userModel;
+    public $kelasModel;
+
+    public function __contruct(){
+        $this->userModel = new UserModel();
+        $this->kelasModel = new Kelas();
+    }
+
+    public function index()
+    {
+        $this->userModel = new UserModel();
+
+        $data = [
+            'title' => 'Create User',
+            'users' => $this->userModel->getUser(),
+        ];
+
+        return view('list_user', $data);
+    }
+
     public function profile($nama = "", $kelas = "", $npm =""){
         $data = [
             'nama' => $nama,
@@ -20,35 +41,46 @@ class UserController extends Controller
     }
 
     public function create(){
-        return view('create_user', ['kelas' => Kelas::all(), ]);
+
+        $kelasModel = new Kelas();
+
+        $kelas = $this->kelasModel->getKelas();
+
+        $data =[
+            'title' => 'Create User',
+            'kelas' => $kelas,
+        ];
+
+        return view('create_user',$data);
+
+        // return view('create_user', ['kelas' => Kelas::all(), ]);
     }
 
-    // public function store(Request $request){
-    //     $data = [
-    //         'nama' => $request->input('nama'),
-    //         'npm' => $request->input('npm'),
-    //         'kelas' => $request->input('kelas')
-    //     ];
-    //     return view('profile', $data);
-
-    // }
-
-    public function store(UserRequest $request){
-        $validatedData = $request->validate([
-            'nama' => 'required|string|max:255',
-            'npm' =>  'required|string|max:255',
-            'kelas_id' =>'required|exists:kelas,id',
+    public function store(Request $request){
+        $this->userModel->create([
+            'nama' => $request->input('nama'),
+            'npm' =>  $request->input('npm'),
+            'kelas_id' => $request->input('kelas_id'),
         ]);
 
-        $user = UserModel::create($validatedData);
+        return redirect()->to('/user');
 
-        $user->load('kelas');
+        // $validatedData = $request->validate([
+        //     'nama' => 'required|string|max:255',
+        //     'npm' =>  'required|string|max:255',
+        //     'kelas_id' =>'required|exists:kelas,id',
+        // ]);
 
-        return view('profile', [
-            'nama' => $user->nama,
-            'npm' => $user->npm,
-            'nama_kelas' => $user->kelas->nama_kelas ?? 'Kelas tidak ditemukan',
-        ]);
+        // $user = UserModel::create($validatedData);
+
+        // $user->load('kelas');
+
+        // return view('profile', [
+        //     'nama' => $user->nama,
+        //     'npm' => $user->npm,
+        //     'nama_kelas' => $user->kelas->nama_kelas ?? 'Kelas tidak ditemukan',
+        // ]);
 
     }
+
 }
--- resources/views/create_user.blade.php
+++ resources/views/create_user.blade.php
@@ -1,16 +1,62 @@
-<!DOCTYPE html>
+@extends('layouts.app')
+
+@section('content')
+<div>
+
+    <div class="d-flex justify-content-center align-items-center min-vh-100">
+        <div class="p-4 border rounded bg-light shadow-sm w-50">
+            <h2 class="text-center mb-4">Form Input Mahasiswa</h2>
+
+        <form action="{{ route('user.store') }}" method="POST">
+            @csrf
+
+            <div class="mb-3">
+                <label for="nama" class="form-label">Nama:</label>
+                <input type="text" name="nama" class="form-control" id="nama">
+                @error('nama')
+                    <div class="text-danger">{{$message}}</div>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <label for="npm" class="form-label">NPM:</label>
+                <input type="text" name="npm" class="form-control" id="npm">
+                @error('npm')
+                    <div class="text-danger">{{$message}}</div>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <label for="id_kelas" class="form-label">Kelas:</label>
+                <select name="kelas_id" id="kelas_id">
+                    @foreach($kelas as $kelasItem) 
+                        <option value="{{$kelasItem->id}}">{{$kelasItem->nama_kelas}}</option>
+                    @endforeach
+                </select>
+                @error('npm')
+                    <div class="text-danger">{{$message}}</div>
+                @enderror
+            </div>
+            <button type="submit" class="btn btn-primary w-100">Submit</button>
+
+        </form>
+    </div>
+</div>
+@endsection
+
+
+<!-- <!DOCTYPE html>
 <html lang="id">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form Input Mahasiswa</title>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"> -->
 
     <!-- Custom CSS -->
-    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
-</head>
-<body>
+    <!-- <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}"> -->
+<!-- </head> -->
+
+<!-- <body>
 
     <div class="d-flex justify-content-center align-items-center min-vh-100">
         <div class="p-4 border rounded bg-light shadow-sm w-50">
@@ -26,5 +72,5 @@
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
+</body> -->
+<!-- </html> -->
